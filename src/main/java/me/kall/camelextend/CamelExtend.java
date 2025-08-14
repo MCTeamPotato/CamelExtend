744338e3754a867dcbe5748ffb7759c0ffddd0f4
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-08-14 16:13:50 +0800

    no more forge config spec

--- src/main/java/me/kall/camelextend/CamelExtend.java
+++ src/main/java/me/kall/camelextend/CamelExtend.java
@@ -1,37 +1,24 @@
 package me.kall.camelextend;
 
-import com.google.common.base.Predicates;
 import com.google.common.collect.Lists;
 import me.kall.camelextend.api.CamelEdible;
-import net.minecraftforge.common.ForgeConfigSpec;
+import me.kall.camelextend.api.JsonConfig;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
+import java.util.Set;
 
 @Mod(CamelExtend.MOD_ID)
 public final class CamelExtend {
     public static final String MOD_ID = "camelextend";
-    public static final String MOD_NAME = "CamelExtend";
-    public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
+
+    public static final JsonConfig CONFIG = JsonConfig.create(MOD_ID, "1.0.0").put("CamelEdible", Lists.newArrayList("minecraft:cactus")).initialize();
+    private static final Set<String> CAMEL_EDIBLE = CONFIG.getSet("CamelEdible", String.class);
 
     public CamelExtend(@NotNull FMLJavaModLoadingContext context) {
-        context.getModEventBus().addListener((FMLCommonSetupEvent event) -> event.enqueueWork(() -> ForgeRegistries.BLOCKS.getEntries().forEach(entry -> ((CamelEdible)entry.getValue()).camelExtend$setIsCamelEdible(CAMEL_EDIBLE.get().contains(entry.getKey().location().toString())))));
-    }
-
-    public static final ForgeConfigSpec CONFIG;
-    public static final ForgeConfigSpec.ConfigValue<List<? extends String>> CAMEL_EDIBLE;
-
-    static {
-        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
-        builder.push(MOD_NAME);
-        CAMEL_EDIBLE = builder.defineList("CamelEdible", Lists.newArrayList("minecraft:cactus"), Predicates.alwaysTrue());
-        builder.pop();
-        CONFIG = builder.build();
+        context.getModEventBus().addListener((FMLCommonSetupEvent event) -> event.enqueueWork(() -> ForgeRegistries.BLOCKS.getEntries().forEach(entry -> ((CamelEdible)entry.getValue()).camelExtend$setIsCamelEdible(CAMEL_EDIBLE.contains(entry.getKey().location().toString())))));
     }
 }
